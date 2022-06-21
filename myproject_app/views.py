88c77266d67cf7f885a89137ba035012c354c449
Deniszharkovgit <107854659+Deniszharkovgit@users.noreply.github.com>
2022-06-21 16:11:14 +0300

    add bot

--- myproject_app/views.py
+++ myproject_app/views.py
@@ -4,10 +4,19 @@ from google.oauth2.service_account import Credentials
 from pycbrf.toolbox import ExchangeRates
 from .models import TableNumbers
 from .serializers import *
-from datetime import datetime
-from .serializers import NumbersSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from notifiers import get_notifier
+from datetime import datetime
+
+
+
+def send_from_user(text_message):
+    token = '5502952184:AAFqjDlmpwl21Lx35PIIyA7Md4EnWAqNypQ'
+    chat_id = '387274652'
+    telegram = get_notifier('telegram')
+    telegram.notify(token=token, chat_id=chat_id, message=text_message)
+
 
 def create_credentials():
     scopes = [
@@ -24,12 +33,15 @@ def create_credentials():
 
 class NumbersList(APIView):
     def get(self, request):
+        now = datetime.now()
         TableNumbers.objects.all().delete()
         rates = ExchangeRates()
         gc = create_credentials()
         gc = gc.open_by_key("1Xc13UDoPCHh1_yQoNwfieFMovmhAcrzsqU_XsQz44SM")
         values = gc.get_worksheet(0).get_all_values()
         for i in range(1, len(values)):
+            if datetime.strptime(values[i][3],'%d.%m.%Y') < now:
+                send_from_user(f'Срок поставки {values[i][1]} истёк')        
             purchase = TableNumbers(number=values[i][0],
             order=values[i][1], 
             price_dollar=values[i][2],
